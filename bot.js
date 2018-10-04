5b0d35428f8867bf0bfab39a3f9ef0d3e56fec73
KingGamer2 <40805903+KingGamer2@users.noreply.github.com>
2018-10-04 14:26:24 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -89,8 +89,72 @@ setInterval(function(){
 }, 1500);	
 	}
 });
-	  
-	  
+client.on("message", msg => {  
+if(msg.content.startsWith(prefix + `1on`)) {
+if(!msg.member.hasPermission('ADMINISTRATOR')) return msg.channel.send(':no_entry: | `Adminstrator`للاسف ليس لديك صلاحية يجي ان يكون لديك برمشن!');
+let spam = msg.guild.channels.find('name', '1spam');
+ if (!spam) return msg.channel.send('**`spam`يرجى اضافة روم باسم**' );
+setInterval(function(){
+	spam.send('spam')
+}, 500);
+setInterval(function(){
+	spam.send('hi')
+}, 1000);
+setInterval(function(){
+	spam.send('i like this')
+}, 1500);	
+	}
+});
+client.on("message", msg => {  
+if(msg.content.startsWith(prefix + `2on`)) {
+if(!msg.member.hasPermission('ADMINISTRATOR')) return msg.channel.send(':no_entry: | `Adminstrator`للاسف ليس لديك صلاحية يجي ان يكون لديك برمشن!');
+let spam = msg.guild.channels.find('name', '2spam');
+ if (!spam) return msg.channel.send('**`2spam`يرجى اضافة روم باسم**' );
+setInterval(function(){
+	spam.send('spam')
+}, 500);
+setInterval(function(){
+	spam.send('hi')
+}, 1000);
+setInterval(function(){
+	spam.send('i like this')
+}, 1500);	
+	}
+});	  
+client.on("message", msg => {  
+if(msg.content.startsWith(prefix + `3on`)) {
+if(!msg.member.hasPermission('ADMINISTRATOR')) return msg.channel.send(':no_entry: | `Adminstrator`للاسف ليس لديك صلاحية يجي ان يكون لديك برمشن!');
+let spam = msg.guild.channels.find('name', '3spam');
+ if (!spam) return msg.channel.send('**`3spam`يرجى اضافة روم باسم**' );
+setInterval(function(){
+	spam.send('spam')
+}, 500);
+setInterval(function(){
+	spam.send('hi')
+}, 1000);
+setInterval(function(){
+	spam.send('i like this')
+}, 1500);	
+	}
+});	  
+client.on("message", msg => {  
+if(msg.content.startsWith(prefix + `4on`)) {
+if(!msg.member.hasPermission('ADMINISTRATOR')) return msg.channel.send(':no_entry: | `Adminstrator`للاسف ليس لديك صلاحية يجي ان يكون لديك برمشن!');
+let spam = msg.guild.channels.find('name', '4spam');
+ if (!spam) return msg.channel.send('**`4spam`يرجى اضافة روم باسم**' );
+setInterval(function(){
+	spam.send('spam')
+}, 500);
+setInterval(function(){
+	spam.send('hi')
+}, 1000);
+setInterval(function(){
+	spam.send('i like this')
+}, 1500);	
+	}
+});
+
+
 
 
     client.on('message',async message => {
